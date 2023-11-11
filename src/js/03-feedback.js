9ffc8d8f075fdd6815e3c2945352487b00f23b25
Mary <32392607+MartFrida@users.noreply.github.com>
2023-11-11 17:17:59 +0100

    corrected 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,44 @@
-import throttle from "lodash.throttle";
+// import throttle from "lodash.throttle";
 
-const feedbackForm = document.querySelector('form.feedback-form');
-const fdbForm = {};
-
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
-
-const fillFeedbackForm = () => {
-  const dataForm = JSON.parse(localStorage.getItem('feedback-form-state'));
-  console.log(dataForm);
-
-  if (dataForm === null) return;
-
-  for (const key in dataForm) {
-    if (dataForm.hasOwnProperty(key)) {
-      feedbackForm.elements[key].value = dataForm[key];
-    }
-  }
-
+const refs = {
+  formEl: document.querySelector('form.feedback-form'),
 }
-fillFeedbackForm();
 
-// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+refs.formEl.addEventListener('input', onFormElement);
 
-feedbackForm.addEventListener('input', throttle((ev) => {
-  fdbForm[ev.target.name] = ev.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(fdbForm));
-}, 500)
+function onFormElement(ev) {
+  const userForm = {};
+  const formData = new FormData(refs.formEl);
+  formData.forEach((value, key) => {
+    userForm[key] = value;
+    saveLocalStorage('feedback-form-state', userForm);
+  })
+}
 
-)
+function saveLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
 
-// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+function getLocalStorage(key) {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (err) {
+    console.log('error', err);
+  }
+}
 
-feedbackForm.addEventListener('submit', (ev) => {
+function onLoad() {
+  const data = getLocalStorage('feedback-form-state') || {};
+  for (const key of Object.keys(data)) {
+    refs.formEl.elements[key].value = data[key];
+  }
+  return data;
+}
+onLoad();
+
+refs.formEl.addEventListener('submit', (ev) => {
   ev.preventDefault();
-  // console.log(localStorage['feedback-form-state']);
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')))
-
-  feedbackForm.reset();
-  // localStorage.clear();
+  console.log(onLoad())
   localStorage.removeItem('feedback-form-state');
-
+  ev.target.reset();
 })
